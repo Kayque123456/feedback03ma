5e09856ee5660464ad3ed57bf8e7c439494b6504
Kayque123456 <jobkayque6@gmail.com>
2025-03-30 18:29:59 -0300

    primeiro commit do projeto e estrutura do HTML

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import iconStar from "./imagens/icon-star.svg"
+
 export function App() {
   return (
     <div>
-      <h1>App</h1>
+      <div>
+        <img src={iconStar} alt="icon star" />
+      </div>
+
+      <h1>How did we do?</h1>
+
+      <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+
+      <div>
+        <input type="button" value={1} />
+        <input type="button" value={2} />
+        <input type="button" value={3} />
+        <input type="button" value={4} />
+        <input type="button" value={5} />
+      </div>
+
+      <button>Submit</button>
     </div>
   )
 }
 
-// 16:34
+// 15:31
