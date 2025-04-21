acbcf76d8c8014fffa75b6ea8f33bd4767287342
Kayque123456 <jobkayque6@gmail.com>
2025-04-20 22:40:46 -0300

    Update InputButton.jsx

--- src/components/InputButton.jsx
+++ src/components/InputButton.jsx
@@ -1,4 +1,4 @@
-export function inputButton({ value, setNotaAvaliacao }) {
+export function InputButton({ value, setNotaAvaliacao }) {
     function handleMudarNotaAvaliacao(nota) {
         setNotaAvaliacao(nota)
     }
