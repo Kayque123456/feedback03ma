5922a90ffd38f5fe08fb69436092a51ba1af9513
Kayque123456 <jobkayque6@gmail.com>
2025-04-20 22:44:32 -0300

    Update InputButton.jsx

--- src/components/InputButton.jsx
+++ src/components/InputButton.jsx
@@ -4,6 +4,6 @@ export function InputButton({ value, setNotaAvaliacao }) {
     }
 
     return (
-        <input type="button" value={value} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white sm:w-13 sm:h-13 hover:bg-orange hover:text-white Sm:w-13 Sm:h-14  hover:cursor-pointer duration-500 font-bold" onClick={() => handleMudarNotaAvaliacao(value)}/>
+        <input type="button" value={value} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white sm:w-13 sm:h-13 hover:bg-orange hover:text-white sm:w-13 sm:h-14  hover:cursor-pointer duration-500 font-bold" onClick={() => handleMudarNotaAvaliacao(value)}/>
     )
 }
